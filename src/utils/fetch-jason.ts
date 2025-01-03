873f4d8673c9206106b853bb57d14707cbadc36d
Queirubim <allan-cebola@hotmail.com>
2025-01-03 20:20:43 -0300

    disable revalidate

--- src/utils/fetch-jason.ts
+++ src/utils/fetch-jason.ts
@@ -1,5 +1,5 @@
 export const fetchJson = async <T>(url: string): Promise<T> => {
-  const rawData = await fetch(url, { next: { revalidate: 36000 } });
+  const rawData = await fetch(url, { next: { revalidate: false } });
 
   const jasonData = await rawData.json();
 
